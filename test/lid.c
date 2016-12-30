e7caa60a7ca5b2607d3c6167a3fb8d100976bc61
James Ye <jye836@gmail.com>
2016-12-30 17:08:41 +1100

    FIXME: improve tests

--- test/lid.c
+++ test/lid.c
@@ -32,7 +32,7 @@
 #include "libinput-util.h"
 #include "litest.h"
 
-START_TEST(lid_open_close)
+START_TEST(lid_switch)
 {
 	struct litest_device *sw = litest_current_device();
 	struct libinput *li = sw->libinput;
@@ -46,7 +46,6 @@ START_TEST(lid_open_close)
 
 	event = libinput_get_event(li);
 	litest_is_switch_event(event, LIBINPUT_SWITCH_STATE_ON);
-
 	libinput_event_destroy(event);
 
 	/* lid opened */
@@ -55,7 +54,6 @@ START_TEST(lid_open_close)
 
 	event = libinput_get_event(li);
 	litest_is_switch_event(event, LIBINPUT_SWITCH_STATE_OFF);
-
 	libinput_event_destroy(event);
 }
 END_TEST
@@ -78,31 +76,114 @@ START_TEST(lid_disable_touchpad)
 	litest_disable_tap(touchpad->libinput_device);
 	litest_drain_events(li);
 
+	/* lid is down - no events */
 	litest_lid_action(sw, LIBINPUT_SWITCH_STATE_ON);
-	libinput_dispatch(li);
-	litest_touch_down(touchpad, 0, 50, 50);
-	litest_touch_move_to(touchpad, 0, 50, 50, 70, 50, 10, 1);
-	litest_touch_up(touchpad, 0);
-
 	litest_assert_only_typed_events(li, LIBINPUT_EVENT_SWITCH_TOGGLE);
 
-	/* lid is down - no events */
 	litest_touch_down(touchpad, 0, 50, 50);
 	litest_touch_move_to(touchpad, 0, 50, 50, 70, 50, 10, 1);
 	litest_touch_up(touchpad, 0);
-
 	litest_assert_empty_queue(li);
 
 	/* lid is up - motion events */
 	litest_lid_action(sw, LIBINPUT_SWITCH_STATE_OFF);
-	libinput_dispatch(li);
-	litest_drain_events(li);
+	litest_assert_only_typed_events(li, LIBINPUT_EVENT_SWITCH_TOGGLE);
 
 	litest_touch_down(touchpad, 0, 50, 50);
 	litest_touch_move_to(touchpad, 0, 50, 50, 70, 50, 10, 1);
 	litest_touch_up(touchpad, 0);
-
 	litest_assert_only_typed_events(li, LIBINPUT_EVENT_POINTER_MOTION);
+
+	litest_delete_device(touchpad);
+}
+END_TEST
+
+START_TEST(lid_disable_touchpad_during_touch)
+{
+	struct litest_device *sw = litest_current_device();
+	struct litest_device *touchpad;
+	struct libinput *li = sw->libinput;
+
+	touchpad = lid_init_paired_touchpad(li);
+	litest_disable_tap(touchpad->libinput_device);
+	litest_drain_events(li);
+
+	litest_touch_down(touchpad, 0, 50, 50);
+	litest_touch_move_to(touchpad, 0, 50, 50, 70, 50, 5, 1);
+	litest_assert_only_typed_events(li, LIBINPUT_EVENT_POINTER_MOTION);
+
+	litest_lid_action(sw, LIBINPUT_SWITCH_STATE_ON);
+	litest_assert_only_typed_events(li, LIBINPUT_EVENT_SWITCH_TOGGLE);
+
+	litest_touch_move_to(touchpad, 0, 70, 50, 50, 50, 5, 1);
+	litest_touch_up(touchpad, 0);
+	litest_assert_empty_queue(li);
+
+	litest_delete_device(touchpad);
+}
+END_TEST
+
+START_TEST(lid_disable_touchpad_edge_scroll)
+{
+	struct litest_device *sw = litest_current_device();
+	struct litest_device *touchpad;
+	struct libinput *li = sw->libinput;
+
+	touchpad = lid_init_paired_touchpad(li);
+	litest_enable_edge_scroll(touchpad);
+
+	litest_drain_events(li);
+
+	litest_lid_action(sw, LIBINPUT_SWITCH_STATE_ON);
+	litest_assert_only_typed_events(li, LIBINPUT_EVENT_SWITCH_TOGGLE);
+
+	litest_touch_down(touchpad, 0, 99, 20);
+	libinput_dispatch(li);
+	litest_timeout_edgescroll();
+	libinput_dispatch(li);
+	litest_assert_empty_queue(li);
+
+	litest_touch_move_to(touchpad, 0, 99, 20, 99, 80, 60, 10);
+	libinput_dispatch(li);
+	litest_assert_empty_queue(li);
+
+	litest_touch_move_to(touchpad, 0, 99, 80, 99, 20, 60, 10);
+	litest_touch_up(touchpad, 0);
+	libinput_dispatch(li);
+	litest_assert_empty_queue(li);
+
+	litest_delete_device(touchpad);
+}
+END_TEST
+
+START_TEST(lid_disable_touchpad_edge_scroll_interrupt)
+{
+	struct litest_device *sw = litest_current_device();
+	struct litest_device *touchpad;
+	struct libinput *li = sw->libinput;
+	struct libinput_event_pointer *stop_event;
+
+	touchpad = lid_init_paired_touchpad(li);
+	litest_enable_edge_scroll(touchpad);
+
+	litest_drain_events(li);
+
+	litest_touch_down(touchpad, 0, 99, 20);
+	libinput_dispatch(li);
+	litest_timeout_edgescroll();
+	litest_touch_move_to(touchpad, 0, 99, 20, 99, 30, 10, 10);
+	libinput_dispatch(li);
+	litest_assert_only_typed_events(li, LIBINPUT_EVENT_POINTER_AXIS);
+
+	litest_lid_action(sw, LIBINPUT_SWITCH_STATE_ON);
+
+	/* scroll stop event */
+	litest_wait_for_event(li);
+	stop_event = litest_is_axis_event(libinput_get_event(li),
+					  LIBINPUT_POINTER_AXIS_SCROLL_VERTICAL,
+					  LIBINPUT_POINTER_AXIS_SOURCE_FINGER);
+	libinput_event_destroy(libinput_event_pointer_get_base_event(stop_event));
+	litest_assert_only_typed_events(li, LIBINPUT_EVENT_SWITCH_TOGGLE);
 
 	litest_delete_device(touchpad);
 }
@@ -111,6 +192,9 @@ END_TEST
 void
 litest_setup_tests_lid(void)
 {
-	litest_add("lid:lid_open_close", lid_open_close, LITEST_SWITCH, LITEST_ANY);
-	litest_add("lid:lid_disable_touchpad", lid_disable_touchpad, LITEST_SWITCH, LITEST_ANY);
+	litest_add("lid:switch", lid_switch, LITEST_SWITCH, LITEST_ANY);
+	litest_add("lid:disable_touchpad", lid_disable_touchpad, LITEST_SWITCH, LITEST_ANY);
+	litest_add("lid:disable_touchpad", lid_disable_touchpad_during_touch, LITEST_SWITCH, LITEST_ANY);
+	litest_add("lid:disable_touchpad", lid_disable_touchpad_edge_scroll, LITEST_SWITCH, LITEST_ANY);
+	litest_add("lid:disable_touchpad", lid_disable_touchpad_edge_scroll_interrupt, LITEST_SWITCH, LITEST_ANY);
 }
